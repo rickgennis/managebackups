c0d8d0a732610733214271076c8c4c41a432ff66
Rick Ennis <3441320+rickgennis@users.noreply.github.com>
2024-03-06 14:38:21 -0500

    added bumping automation to setVersion

--- include/globalsdef.h
+++ include/globalsdef.h
@@ -2,7 +2,7 @@
 #ifndef GLOBALSDEF_H
 #define GLOBALSDEF_H
 
-#define VERSION "1.6.9"
+#define VERSION "1.6.9a"
 
 #include "cxxopts.hpp"
 #include "colors.h"
--- src/managebackups.1.md
+++ src/managebackups.1.md
@@ -1,4 +1,4 @@
-% MANAGEBACKUPS(1) managebackups 1.6.9
+% MANAGEBACKUPS(1) managebackups 1.6.9a
 % Rick Ennis
 % March 2023
 
